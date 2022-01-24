8e76b7ca3d472f7ce70bcf78f0943623f3d4ce52
Bailey Yu <bailey.yu@yahoo.com>
2022-01-23 21:18:53 -0800

    finished dropdown widget

---
+++ src/components/dropdown.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect, useRef} from "react";
+
+const Dropdown = ({options, selected, onSelectedChange}) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+  useEffect(() => {
+    const bodyClick = (event) => {
+      if(!ref.current.contains(event.target)) setOpen(false)
+    }
+    document.body.addEventListener('click', bodyClick)
+
+    return () => {
+      document.body.removeEventListener(bodyClick);
+    }
+  }, [])
+  const renderedOptions = options.map((option => {
+    if(selected === option)return null;
+    return (
+      <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
+        {option.label}
+      </div>
+    );
+  }))
+
+  return (
+    <div className="ui form" ref={ref}>
+      <div className="field">
+        <label htmlFor="" className="label">Select a Color</label>
+        <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''} `}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dropdown;
