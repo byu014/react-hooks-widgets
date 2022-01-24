c3b60c877b08d61a6b2cb8b0ab620217d2e8134e
Bailey Yu <bailey.yu@yahoo.com>
2022-01-23 17:45:02 -0800

    added wikipedia search widget

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,12 +1,55 @@
 import React, {useState, useEffect} from "react";
-
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('react');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([])
 
   useEffect(() => {
-    console.log('rendered')
-  },[])
+    const timeoutId = setTimeout(() => {
+      setDebouncedTerm(term);
+    },500);
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [term])
+
+  useEffect(() => {
+    const search = async () => {
+      const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params:{
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm
+        }
+      })
+      setResults(data.query.search);
+    }
+
+    search();
+  },[debouncedTerm])
+
+
+  const renderedResults = results.map(result => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated floated">
+          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div>
       <div className="ui form">
@@ -17,6 +60,9 @@ const Search = () => {
           className="input"
           value={term}
           onChange={e => setTerm(e.target.value)} />
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
